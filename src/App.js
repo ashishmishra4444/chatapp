f16100a55212ff2165a2084869f02d97d8451b85
Ashish Kumar Mishra <ashishanil334@gmail.com>
2024-09-07 11:06:48 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 // import socketIO from "socket.io-client";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Join from "./component/Join/Join";
+import Chat from "./component/Chat/Chat";
 
 // const ENDPOINT="http://localhost:4500/"
 // const socket = socketIO(ENDPOINT,{transports:["websocket"]});
@@ -12,7 +13,7 @@ function App() {
       <Router>
         <Routes>
           <Route exact path="/" Component={Join} />
-          <Route path="/chat" />
+          <Route path="/chat" Component={Chat} />
         </Routes>
       </Router>
     </div>
--- src/component/Join/Join.js
+++ src/component/Join/Join.js
@@ -1,18 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Join.css";
 import logo from "../../images/logo.png";
+import { Link } from "react-router-dom";
+
+let user;
+
+const sendUser = () => {
+  user = document.getElementById("joinInput").value;
+  document.getElementById("joinInput").value = "";
+};
 
 const Join = () => {
+
+  const[name,setName]=useState("");
+//   console.log(name);
+
   return (
     <div className="JoinPage">
       <div className="JoinContainer">
         <img src={logo} alt="Logo" />
         <h1>C CHAT</h1>
-        <input type="text" id="joinInput" />
-        <button className="joinbtn">Join</button>
+        <input onChange={(e)=>setName(e.target.value)} placeholder="Enter your name" type="text" id="joinInput" />
+        <Link onClick={(event)=>!name?event.preventDefault():null} to="/chat">
+          <button onClick={sendUser} className="joinbtn">
+            Join
+          </button>
+        </Link>
       </div>
     </div>
   );
 };
 
 export default Join;
+export { user };
